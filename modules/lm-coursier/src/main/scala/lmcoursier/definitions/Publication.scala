0704e58483f1b6e7021573cea28e68af2fb8dd65
Eugene Yokota <eed3si9n@gmail.com>
2019-08-04 01:04:06 -0400

    re-enable sbt-contraband, and deal with Dependency change

--- modules/lm-coursier/src/main/scala/lmcoursier/definitions/Publication.scala
+++ modules/lm-coursier/src/main/scala/lmcoursier/definitions/Publication.scala
@@ -8,23 +8,23 @@ final class Publication private (
   val name: String,
   val `type`: Type,
   val ext: Extension,
-  val classifier: Classifier) extends Serializable {
+  val classifier: Classifier,
+  val attributes: Attributes) extends Serializable {
   
-  def attributes: Attributes =
-    Attributes(`type`, classifier)
+  private def this(name: String, `type`: Type, ext: Extension, classifier: Classifier) = this(name, `type`, ext, classifier, Attributes(`type`, classifier))
   
   override def equals(o: Any): Boolean = o match {
-    case x: Publication => (this.name == x.name) && (this.`type` == x.`type`) && (this.ext == x.ext) && (this.classifier == x.classifier)
+    case x: Publication => (this.name == x.name) && (this.`type` == x.`type`) && (this.ext == x.ext) && (this.classifier == x.classifier) && (this.attributes == x.attributes)
     case _ => false
   }
   override def hashCode: Int = {
-    37 * (37 * (37 * (37 * (37 * (17 + "lmcoursier.definitions.Publication".##) + name.##) + `type`.##) + ext.##) + classifier.##)
+    37 * (37 * (37 * (37 * (37 * (37 * (17 + "lmcoursier.definitions.Publication".##) + name.##) + `type`.##) + ext.##) + classifier.##) + attributes.##)
   }
   override def toString: String = {
-    "Publication(" + name + ", " + `type` + ", " + ext + ", " + classifier + ")"
+    "Publication(" + name + ", " + `type` + ", " + ext + ", " + classifier + ", " + attributes + ")"
   }
-  private[this] def copy(name: String = name, `type`: Type = `type`, ext: Extension = ext, classifier: Classifier = classifier): Publication = {
-    new Publication(name, `type`, ext, classifier)
+  private[this] def copy(name: String = name, `type`: Type = `type`, ext: Extension = ext, classifier: Classifier = classifier, attributes: Attributes = attributes): Publication = {
+    new Publication(name, `type`, ext, classifier, attributes)
   }
   def withName(name: String): Publication = {
     copy(name = name)
@@ -38,8 +38,12 @@ final class Publication private (
   def withClassifier(classifier: Classifier): Publication = {
     copy(classifier = classifier)
   }
+  def withAttributes(attributes: Attributes): Publication = {
+    copy(attributes = attributes)
+  }
 }
 object Publication {
   
   def apply(name: String, `type`: Type, ext: Extension, classifier: Classifier): Publication = new Publication(name, `type`, ext, classifier)
+  def apply(name: String, `type`: Type, ext: Extension, classifier: Classifier, attributes: Attributes): Publication = new Publication(name, `type`, ext, classifier, attributes)
 }
